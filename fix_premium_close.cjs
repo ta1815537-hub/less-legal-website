69232d14d7850ca20c5d8d748aa7b63c35232166
ta1815537-hub <ta1815537@gmail.com>
2026-09-01 20:23:19 +0530

    build: refactor project configuration and cleanup
    - Move build/dev tools to devDependencies
    - Add wrangler for Cloudflare deployment
    - Remove legacy maintenance scripts

--- fix_premium_close.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/pages/PremiumPage.tsx', 'utf8');
-content = content.replace(
-  '            </motion.div>\n          </div>\n        </ScrollReveal>\n      </div>\n\n      {/* Mandatory PayU / Merchant Verification Compliance Links */}',
-  '            </motion.div>\n          </div>\n          </div>\n        </ScrollReveal>\n      </div>\n\n      {/* Mandatory PayU / Merchant Verification Compliance Links */}'
-);
-fs.writeFileSync('src/pages/PremiumPage.tsx', content);
--- fix_premium_close2.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/pages/PremiumPage.tsx', 'utf8');
-
-content = content.replace(
-  '            </div>\n            <motion.div \n              initial={{ scale: 0.9 }}',
-  '            </div>\n          </div>\n            <motion.div \n              initial={{ scale: 0.9 }}'
-);
-
-fs.writeFileSync('src/pages/PremiumPage.tsx', content);
